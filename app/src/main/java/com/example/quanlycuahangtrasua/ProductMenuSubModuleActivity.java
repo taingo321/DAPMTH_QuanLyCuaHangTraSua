58ceef2128a0b0dcbdf0cab7d4135f2e990658bb
dinhduc24k322 <dinhducphan02@gmail.com>
2023-10-26 13:58:52 +0700

    NTCD-25 Product sub module
    Thiết kế giao diện Sub-Module của Sản phẩm

---
+++ app/src/main/java/com/example/quanlycuahangtrasua/ProductMenuSubModuleActivity.java
@@ -0,0 +1,14 @@
+package com.example.quanlycuahangtrasua;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProductMenuSubModuleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product_menu_sub_module);
+    }
+}
