6113534034a02ddb24d4fbffb0e92e20ff3ed686
Hải Phong <nguyenquochaiphong2701@gmail.com>
2023-10-26 18:27:54 +0700

    NTCD-32 add function register

--- app/src/main/java/com/example/quanlycuahangtrasua/RegisterActivity.java
+++ app/src/main/java/com/example/quanlycuahangtrasua/RegisterActivity.java
@@ -6,9 +6,102 @@ import android.os.Bundle;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    private Button register_button;
+    private EditText register_username_input, register_phone_input, register_password_input;
+    private ProgressDialog loadingBar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        register_button = findViewById(R.id.register_button);
+        register_username_input = findViewById(R.id.register_username_input);
+        register_phone_input = findViewById(R.id.register_phone_input);
+        register_password_input = findViewById(R.id.register_password_input);
+        loadingBar = new ProgressDialog(this);
+
+        register_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CreateAccount();
+            }
+        });
     }
+
+    private void CreateAccount() {
+        String username = register_username_input.getText().toString();
+        String phone = register_phone_input.getText().toString();
+        String password = register_password_input.getText().toString();
+
+        if (TextUtils.isEmpty(username)){
+            Toast.makeText(this, "Vui lòng nhập tên tài khoản", Toast.LENGTH_SHORT).show();
+        }
+
+        else if (TextUtils.isEmpty(phone)){
+            Toast.makeText(this, "Vui lòng nhập số điện thoại", Toast.LENGTH_SHORT).show();
+        }
+
+        else if (TextUtils.isEmpty(password)){
+            Toast.makeText(this, "Vui lòng nhập mật khẩu", Toast.LENGTH_SHORT).show();
+        }
+
+        else {
+            loadingBar.setTitle("Tạo mật khẩu");
+            loadingBar.setMessage("Vui lòng chờ xác nhận");
+            loadingBar.setCanceledOnTouchOutside(false);
+            loadingBar.show();
+
+            ValidateUsername(username, phone, password);
+        }
+    }
+
+    private void ValidateUsername(String username, String phone, String password) {
+        final DatabaseReference RootRef;
+        RootRef = FirebaseDatabase.getInstance().getReference();
+
+        RootRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (!(snapshot.child("Users").child(username).exists())){
+                    HashMap<String, Object> userdataMap = new HashMap<>();
+                    userdataMap.put("phone", phone);
+                    userdataMap.put("password", password);
+                    userdataMap.put("username", username);
+
+                    RootRef.child("Users").child(username).updateChildren(userdataMap)
+                            .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if (task.isSuccessful()){
+                                        Toast.makeText(Register.this, "Tài khoản của bạn đã tạo thành công", Toast.LENGTH_SHORT).show();
+                                        loadingBar.dismiss();
+
+                                        Intent intent = new Intent(Register.this, Login.class);
+                                        startActivity(intent);
+                                    }
+                                    else {
+                                        loadingBar.dismiss();
+                                        Toast.makeText(Register.this, "Đã xảy ra lỗi,vui lòng thử lại", Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+                }
+                else {
+                    Toast.makeText(Register.this, "Tên tài khoản " + phone + " đã tồn tại.", Toast.LENGTH_SHORT).show();
+                    loadingBar.dismiss();
+                    Toast.makeText(Register.this, "Vui lòng thử lại", Toast.LENGTH_SHORT).show();
+
+                    Intent intent = new Intent(Register.this, Login.class);
+                    startActivity(intent);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
 }
