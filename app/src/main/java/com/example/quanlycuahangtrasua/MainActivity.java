4c9b103453d80358660bef2768f6ef37fad2dad5
Hải Phong <nguyenquochaiphong2701@gmail.com>
2023-10-27 20:39:28 +0700

    NTCD-28 Switch product layout

--- app/src/main/java/com/example/quanlycuahangtrasua/MainActivity.java
+++ app/src/main/java/com/example/quanlycuahangtrasua/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.quanlycuahangtrasua;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    ImageView ivProduct, ivCart, ivInvoice, ivStaff;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ivProduct = findViewById(R.id.ivProduct);
+        ivCart = findViewById(R.id.ivCart);
+        ivInvoice = findViewById(R.id.ivInvoice);
+        ivStaff = findViewById(R.id.ivStaff);
+
+        ivProduct.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ProductMenuSubModule.class);
+                startActivity(intent);
+            }
+        });
     }
 }
