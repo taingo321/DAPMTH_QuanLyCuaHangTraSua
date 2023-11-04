7daf836533cb854ff670ca80444e0d44e1c63e60
Hải Phong <nguyenquochaiphong2701@gmail.com>
2023-11-04 14:41:51 +0700

    NTCD-49 Fix

--- app/src/main/java/com/example/quanlycuahangtrasua/ProductManagementActivity.java
+++ app/src/main/java/com/example/quanlycuahangtrasua/ProductManagementActivity.java
@@ -1,30 +1,85 @@
 package com.example.quanlycuahangtrasua;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 
+import com.example.quanlycuahangtrasua.Model.Products;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
-public class ProductManagementActivity extends AppCompatActivity {
+public class ProductManagement extends AppCompatActivity {
 
     private FloatingActionButton fabAddProduct;
+    private DatabaseReference ProductsManagementRef;
+    private RecyclerView rvProductListManagement;
+    RecyclerView.LayoutManager layoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product_management);
 
+        ProductsManagementRef = FirebaseDatabase.getInstance().getReference().child("Products");
+        rvProductListManagement = findViewById(R.id.rvProductListManagement);
+        rvProductListManagement.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(this);
+        rvProductListManagement.setLayoutManager(layoutManager);
+
         fabAddProduct = findViewById(R.id.fabAddProduct);
         fabAddProduct.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(ProductManagementActivity.this, AddNewProductActivity.class);
+                Intent intent = new Intent(ProductManagement.this, AddNewProductActivity.class);
                 startActivity(intent);
             }
         });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseRecyclerOptions<Products> options = new FirebaseRecyclerOptions.Builder<Products>()
+                .setQuery(ProductsManagementRef, Products.class).build();
+
+        FirebaseRecyclerAdapter<Products, ProductViewHolder> adapter = new FirebaseRecyclerAdapter<Products, ProductViewHolder>(options) {
+            @Override
+            protected void onBindViewHolder(@NonNull ProductViewHolder holder, int position, @NonNull Products model) {
+                holder.productName.setText(model.getPname());
+                holder.productPrice.setText(model.getPrice());
+                holder.productIngre.setText(model.getPrice());
+                Picasso.get().load(model.getImage()).into(holder.productImage);
+
+                holder.itemView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent intent = new Intent(ProductManagement.this, ProductDetail.class);
+                        intent.putExtra("pid", model.getPid());
+                        startActivity(intent);
+                    }
+                });
+            }
+
+            @NonNull
+            @Override
+            public ProductViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.product_layout, parent, false);
+                ProductViewHolder holder = new ProductViewHolder(view);
+                return holder;
+            }
+        };
+        rvProductListManagement.setAdapter(adapter);
+        adapter.startListening();
     }
 }
