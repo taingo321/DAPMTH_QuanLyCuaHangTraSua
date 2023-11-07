69fc896136f6601a7fc6c420b28d6da96a1fa1bf
taingo321 <anhtaingo962@gmail.com>
2023-11-07 21:01:30 +0700

    NTCD-66 Update Login for Admin and Staff

---
+++ app/src/main/java/com/example/quanlycuahangtrasua/Prevalent/Prevalent.java
@@ -0,0 +1,10 @@
+package com.example.quanlycuahangtrasua.Prevelent;
+
+import com.example.quanlycuahangtrasua.Model.Users;
+
+public class Prevelent {
+    public static Users currentOnlineUser;
+
+    public static final String UserNameKey = "Username";
+    public static final String UserPasswordKey = "UserPassword";
+}
