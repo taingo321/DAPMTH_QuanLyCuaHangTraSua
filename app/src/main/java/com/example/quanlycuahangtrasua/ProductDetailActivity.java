ae15cb259b6c7e2654d8a3bf3828368b1e5eb21c
taingo321 <anhtaingo962@gmail.com>
2023-11-07 16:00:35 +0700

    NTCD-64 Fix/Update all View Product Detail Function
    - Product For Cart Detail and Product For Management Detail are now usable

--- app/src/main/java/com/example/quanlycuahangtrasua/ProductDetailActivity.java
+++ app/src/main/java/com/example/quanlycuahangtrasua/ProductDetailActivity.java
@@ -1,8 +1,19 @@
 package com.example.quanlycuahangtrasua;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.quanlycuahangtrasua.Model.Products;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
 
 public class activity_product_detail extends AppCompatActivity {
 
---
+++ app/src/main/java/com/example/quanlycuahangtrasua/ViewHolder/ProductViewHolder.java
@@ -0,0 +1,2 @@
+package com.example.quanlycuahangtrasua.ViewHolder;public class ProductViewHolder {
+}
